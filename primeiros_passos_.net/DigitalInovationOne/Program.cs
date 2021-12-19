8337572cfd02db07a15d7be1b52eeb874500f702
freshSant <gustavo.git@hotmail.com>
2021-12-19 16:24:40 -0300

    [primeiros_pasos_.net] Atualização resumo, e prática, tópico 1 e 2 completos

---
+++ primeiros_passos_.net/DigitalInovationOne/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DigitalInovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeVezes = 100;
+
+            for(int i = 0; i < numeroDeVezes; i++){
+                Console.WriteLine($"Vou entrar na Avanade, Amém! {i}\n");
+            }
+        }
+    }
+}
